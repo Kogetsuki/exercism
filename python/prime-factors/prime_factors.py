acd71d335ffdba71c4ba84583959fd82be907294
Kogetsuki <rozenbastien@gmail.com>
2026-03-03 18:31:19 +0100

    prime-factors/

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,17 @@
+def factors(value):
+  result = []
+  
+  candidate = 2
+  
+  while candidate <= value:
+    if value % candidate == 0:
+      result.append(candidate)
+      value //= candidate
+    
+    else:
+      candidate += 1
+
+  return result
+
+
+print(factors(2))
