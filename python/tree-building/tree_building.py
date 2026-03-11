65070e43ac941b6ec2ca2d3bbc488a7916222a65
Kogetsuki <rozenbastien@gmail.com>
2026-03-11 11:28:00 +0100

    tree-building/

--- python/tree-building/tree_building.py
+++ python/tree-building/tree_building.py
@@ -15,33 +15,30 @@ def BuildTree(records):
     return None
 
   # Sort records by record_id
-  records.sort(key=lambda r: r.record_id)
+  records.sort(
+    key=lambda r: r.record_id
+  )
 
-  # Validate IDs
-  for i, record in enumerate(records):
-    if record.record_id != i:
+  nodes = []
+
+  for i, r in enumerate(records):
+    # Validation
+    if r.record_id != i:
       raise ValueError("Record id is invalid or out of order.")
 
-    if record.record_id == 0:
-      if record.parent_id != 0:
-        raise ValueError("Node parent_id should be smaller than its record_id.")
+    if r.record_id == 0 and r.parent_id != 0:
+      raise ValueError("Node parent_id should be smaller than its record_id.")
 
-    else:
-      if record.record_id == record.parent_id:
+    if r.record_id != 0:
+      if r.record_id == r.parent_id:
         raise ValueError("Only root should have equal record and parent id.")
 
-      if record.parent_id >= record.record_id:
+      if r.parent_id >= r.record_id:
         raise ValueError("Node parent_id should be smaller than its record_id.")
 
-  # Create nodes
-  nodes = [
-    Node(r.record_id)
-    for r in records
-  ]
-
-  # Link parent and children
-  for record in records[1:]:    # skip root
-    parent_node = nodes[record.parent_id]
-    parent_node.children.append(nodes[record.record_id])
+    # Create node and link to parent
+    nodes.append(Node(r.record_id))
+    if r.record_id > 0:
+      nodes[r.parent_id].children.append(nodes[i])
 
   return nodes[0]   # root
